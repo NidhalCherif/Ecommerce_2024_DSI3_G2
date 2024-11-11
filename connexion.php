9abf3d794a9c3570592671e90357ce439f4cb240
Nidhal CHERIF <cherif.nidhal@gmail.com>
2024-11-11 15:33:56 +0100

    version2 : classe connexion ,class produit et class crudProduit

---
+++ connexion.php
@@ -0,0 +1,17 @@
+<?php
+class connexion
+{
+    private $pdo;
+    public function __construct()
+    {
+
+        $dsn = "mysql:host=localhost;dbname=ecommerce_l2dsi3_g2";
+        $user = "root";
+        $pw = "";
+        $this->pdo = new PDO($dsn, $user, $pw);
+    }
+    public function getConnexion()
+    {
+        return $this->pdo;
+    }
+}
